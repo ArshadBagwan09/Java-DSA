6c432b85c0e7b2356048780d60e37e1d33a6cbf3
Hallo Arshad <bagwanarshad95@gmail.com>
2026-06-08 20:49:47 +0530

    File in rename

--- Pattern/Pattern2.java
+++ Pattern/Pattern2.java
@@ -1,18 +1,21 @@
-package Patter;
+package Pattern;
 
-import java.util.*;;
+import java.util.*;
 
 public class Pattern2 {
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int n;
-    n = sc.nextInt();
+    System.out.println("Enter Number");
+    int n = sc.nextInt();
     for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n - i; j++) {
+        System.out.print(" ");
+      }
       for (int j = 1; j <= i; j++) {
         System.out.print("*");
       }
       System.out.println(" ");
     }
-
   }
 }
